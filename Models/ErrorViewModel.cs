f58787077588ec6f283b1206fd8ea2e4a7ed7874
Damian Pietrucha <pietruszynski.damian@gmail.com>
2023-04-24 11:38:51 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHome_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
