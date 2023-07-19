45c57a22b847555bc0a28f37dd00f09a69496705
Damian Pietrucha <pietruszynski.damian@gmail.com>
2023-07-19 15:50:52 +0200

    First traying with Charts.

--- Controllers/TablesController.cs
+++ Controllers/TablesController.cs
@@ -6,6 +6,8 @@ namespace SmartHome_MVC.Controllers
 {
     public class TablesController : Controller
     {
+        public static DateTime selectedDateStatic = DateTime.Now;
+
         public IActionResult Tables()
         {
             using (SmartHomeDbContext dbContext = new SmartHomeDbContext())
@@ -19,16 +21,39 @@ namespace SmartHome_MVC.Controllers
                     .ToList();
 
                 TablesViewModel viewModel = new TablesViewModel();
-                viewModel.ReadValuesFromArduinoViewModel = table;
+                viewModel.ReadValuesFromArduinoViewModel = last150Records;
 
                 return View(viewModel);
 
             }
         }
-            public ActionResult SetSelectedDate(int index, string selectedDate)
-            {
+        public ActionResult SetSelectedDate(int index, DateTime selectedDate)
+        {
+            selectedDateStatic = selectedDate;
+            return Json(new { success = true });
+        }
 
-                return Json(new { success = true });
+        public ActionResult GetData(DateTime selectedDate)
+        {
+            using (SmartHomeDbContext dbContext = new SmartHomeDbContext())
+            {
+                List<ReadValuesFromArduino> table = dbContext.ReadValuesArduino.ToList();
+  
+                List<ReadValuesFromArduino> selectedDateTable = table
+                    .Where(k => k.DateTime.Date == selectedDate.Date)
+                    .ToList();
+
+                TablesViewModel viewModel = new TablesViewModel();
+                viewModel.ReadValuesFromArduinoViewModel = selectedDateTable;
+
+                return PartialView("_TablePartial", viewModel);
             }
         }
+
+
+
+
+
+
+    }
     } 
--- Controllers/TrendsController.cs
+++ Controllers/TrendsController.cs
@@ -1,11 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartHome_Database;
+using SmartHome_Database.Enities;
+using System.Diagnostics.Metrics;
 
 namespace SmartHome_MVC.Controllers
 {
     public class TrendsController : Controller
     {
+        private readonly SmartHomeDbContext _dbContext;
+        public TrendsController(SmartHomeDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Trends()
         {
+            // Pobierz dane z bazy
+            List<ReadValuesFromArduino> measurements = _dbContext.ReadValuesArduino.ToList();
+
+            // Przygotuj dane do wykresów
+            var timeLabels = measurements.Select(m => m.DateTime.ToString("yyyy-MM-dd HH:mm:ss")).ToList();
+            var temperatureData = measurements.Select(m => m.temperature).ToList();
+            var button1Data = measurements.Select(m => m.button1).ToList();
+            var button2Data = measurements.Select(m => m.button2).ToList();
+            var analogData = measurements.Select(m => m.analog).ToList();
+
+            ViewBag.TimeLabels = timeLabels;
+            ViewBag.TemperatureData = temperatureData;
+            ViewBag.Button1Data = button1Data;
+            ViewBag.Button2Data = button2Data;
+            ViewBag.AnalogData = analogData;
+
             return View();
         }
     }
